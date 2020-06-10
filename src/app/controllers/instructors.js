14a6f7e6042de788d062e3566825a50bf4447890
Natanael Saymon <nsaymonsouza@gmail.com>
2020-06-10 20:41:31 -0300

    Done: exibição de dados e criação de novos instruc

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,13 +1,16 @@
-const { age, date } = require('../../lib/utils')
-const db = require('../../config/db')
+const Instructor = require('../models/Instructor')
 
 module.exports = {
   index(req, res){
-    return res.render('instructors/index')
+    Instructor.all(function(instructors){
+      return res.render("instructors/index", { instructors })
+    })
   },
+
   create(req, res){
     return res.render('instructors/create')
   },
+
   post(req, res){
     const keys = Object.keys(req.body)
     for(key of keys){
@@ -15,43 +18,20 @@ module.exports = {
         return res.send("Por favor, preencha todos os campos!")
       }
     }
-    
-    const query = `
-      INSERT INTO instructors (
-        name,
-        avatar_url,
-        gender,
-        services,
-        birth,
-        created_at
-      ) VALUES ($1, $2, $3, $4, $5, $6)
-      RETURNING id
-    `
-
-    const values = [
-      req.body.name,
-      req.body.avatar_url,
-      req.body.gender,
-      req.body.services,
-      date(req.body.birth).iso, //Para fazer o envio do birth, teremos que passar em formato ISO. 
-      date(Date.now()).iso
-    ]
-
-    db.query(query, values, function(err, results){
-      if(err) {
-        return res.send("Database Error!")
-      }
-      
-      return res.redirect(`/instructors/${results.rows[0].id}`)
+    Instructor.create(req.body, function(instructor){
+      res.redirect(`/instructors/${instructor.id}`)
     })
     
   },
+
   show(req, res){
     return
   },
+
   edit(req, res){
     return
   },
+
   put(req, res){
     const keys = Object.keys(req.body)
     for(key of keys){
@@ -62,6 +42,7 @@ module.exports = {
 
     return
   },
+  
   delete(req, res){
     return
   }
