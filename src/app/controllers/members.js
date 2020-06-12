f968a9699cd4265d6f943e59eadf49d00341f382
Natanael Saymon <nsaymonsouza@gmail.com>
2020-06-12 14:42:50 -0300

    Done: criação, exibção e edição de membros

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -1,8 +1,11 @@
+const Member = require('../models/Member')
 const { age, date } = require('../../lib/utils')
 
 module.exports = {
   index(req, res){
-    return res.render('members/index')
+    Member.all(function(members){
+      return res.render('members/index', { members })
+    })
   },
   create(req, res){
     return res.render('members/create')
@@ -14,14 +17,31 @@ module.exports = {
         return res.send("Por favor, preencha todos os campos!")
       }
     }
-    let { avatar_url, name, birth, services, gender } = req.body
+    Member.create(req.body, function(member){
+      res.redirect(`/members/${member.id}`)
+    })
     
   },
   show(req, res){
-    return
+    Member.find(req.params.id, function(member){
+      if(!member){
+        return res.send("Membro não encontrado, tente novamente!")
+      }
+
+      member.birth = date(member.birth).birthDay
+
+      return res.render("members/show", { member })
+    })
   },
   edit(req, res){
-    return
+    Member.find(req.params.id, function(member){
+      if(!member){
+        return res.send("Membro não encontrado, tente novamente!")
+      }
+
+      member.birth = date(member.birth).iso
+      return res.render("members/edit", { member })
+    })
   },
   put(req, res){
     const keys = Object.keys(req.body)
@@ -31,9 +51,13 @@ module.exports = {
       }
     }
 
-    return
+    Member.update(req.body, function(){
+      return res.redirect(`/members/${req.body.id}`)
+    })
   },
   delete(req, res){
-    return
+    Member.delete(req.body.id, function(){
+      return res.redirect("/members")
+    })
   }
 }
---
+++ src/app/models/Member.js
@@ -0,0 +1,93 @@
+const db = require('../../config/db')
+const { age, date } = require('../../lib/utils')
+
+module.exports = {
+  //Funções padrões
+  all(callback){
+    db.query(`SELECT * FROM members`, function(err, results){
+      if(err){
+        throw `Database Error! ${err}`
+      }
+      callback(results.rows)
+    })
+  },
+
+  create(data, callback){
+    const query = `
+      INSERT INTO members (
+        avatar_url,
+        name,
+        email,
+        birth,
+        gender,
+        blood,
+        weight,
+        height
+      ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+      RETURNING id
+    `
+
+    const values = [
+      data.avatar_url,
+      data.name,
+      data.email,
+      date(data.birth).iso,
+      data.gender,
+      data.blood,
+      data.weight,
+      data.height
+    ]
+
+    db.query(query, values, function(err, results){
+      if(err){
+        throw `Database Error! ${err}`
+      }
+
+      callback(results.rows[0])
+    })
+  },
+
+  find(id, callback){
+    db.query(`SELECT * FROM members WHERE id = $1`, [id], function(err, results){
+      if(err){
+        throw `Database Error! ${err}`
+      }
+      callback(results.rows[0])
+    })
+  },
+
+  update(data, callback){
+    const query = `
+      UPDATE members SET 
+        avatar_url=($1),
+        name=($2),
+        email=($3),
+        birth=($4),
+        gender=($5),
+        blood=($6),
+        weight=($7),
+        height=($8)
+      WHERE id = $9
+    `
+
+    const values = [
+      data.avatar_url,
+      data.name,
+      data.email,
+      date(data.birth).iso,
+      data.gender,
+      data.blood,
+      data.weight,
+      data.height,
+      data.id
+    ]
+
+    db.query(query, values, function(err, results){
+      if(err){
+        throw `Database Error! ${err}`  
+      }
+
+      callback()
+    })
+  }
+}
