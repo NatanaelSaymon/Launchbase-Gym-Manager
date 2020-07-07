5651a5e461d71f54f5d86f1be7ab37fac2df39e8
Natanael Saymon <nsaymonsouza@gmail.com>
2020-07-07 20:58:01 -0300

    Done: script de paginação

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,4 @@
+//Script para inserir class ACTIVE no menu
 const currentPage = location.pathname
 const menuItens = document.querySelectorAll("header .links a")
 
@@ -6,4 +7,33 @@ for(item of menuItens){
         item.classList.add('active')
     }
 }
+
+//Script de Paginação
+let totalPages = 20
+let selectedPage = 16
+let pages = []
+let oldPage
+
+for(let currentPage = 1; currentPage <= totalPages; currentPage++){
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+
+    if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage){
+
+        if(oldPage && currentPage - oldPage > 2){
+            pages.push("...")
+        }
+
+        if(oldPage && currentPage - oldPage  == 2){
+            pages.push(oldPage + 1)
+        }
+
+        pages.push(currentPage)
+
+        oldPage = currentPage
+    }
+}
+
+console.log(pages)
 
